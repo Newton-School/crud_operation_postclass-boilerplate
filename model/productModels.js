40822217d5139b12f171c4ce87d99b57545fa25d
MannShah17 <mannshah2004@gmail.com>
2023-09-14 15:18:20 +0530

    done

---
+++ model/productModels.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+// Define the product schema
+const productSchema = new mongoose.Schema({
+  // Implement the product schema fields here as
+  // productName as String and as a price: Number
+});
+
+// Create and export the Product model
